5473dfcebc53c84202096d5119ca0b4b4fce1ced
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-05-21 15:38:01 +0100

    Movies page

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#autoWidth,#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth,#autoWidth2').removeClass('cs-hidden');
+        } 
+    });  
+  });
